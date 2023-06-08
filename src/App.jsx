798c77e423592c5bc123a7e81e7c4acd4df77dda
Kunniii <nguyenairport56@gmail.com>
2023-06-09 05:29:48 +0700

    init, complete

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import * as cheerio from "cheerio";
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
+
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [rawRSS, setRawRSS] = useState("");
+  useEffect(() => {
+    fetch("http://codechovui.dev/index.xml")
+      .then((response) => response.text())
+      .then((rawRSS) => {
+        setRawRSS(rawRSS);
+        let $ = cheerio.load(rawRSS, {
+          xmlMode: true,
+        });
+        let tempPosts = [];
+        for (let item of $("item")) {
+          let postTitle = $(item).find("title").text();
+          let postUrl = $(item).find("link").text();
+          let pubDate = $(item).find("pubDate").text();
+          let description = $(item).find("description").text();
+          tempPosts.push({ postTitle, postUrl, description, pubDate });
+        }
+        let parsedDatePosts = tempPosts.map((post) => {
+          return { ...post, pubDate: new Date(post.pubDate) };
+        });
+        parsedDatePosts.sort((b, a) => a.pubDate - b.pubDate);
+        const myPosts = parsedDatePosts.map((post) => {
+          return { ...post, pubDate: post.pubDate.toDateString() };
+        });
+        setPosts(myPosts);
+      });
+  }, []);
+
+  return (
+    <div className="bg-slate-900 select-none">
+      <h1 className="text-center text-white pt-24 font-bold text-7xl">RSS Code Cho Vui</h1>
+      <div className="grid grid-cols-3 px-64 py-20">
+        {posts.map((post) => (
+          <Card {...post} />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,33 @@
+export default function Card({ postTitle, postUrl, description, pubDate }) {
+  return (
+    <div className="m-2 px-2 py-4 rounded-lg hover:scale-105 duration-200 bg-white hover:bg-red-500 hover:text-white">
+      <div>
+        <a
+          href={postUrl}
+          className="font-bold text-xl"
+          target="_blank"
+        >
+          {postTitle}
+        </a>
+        <hr className="my-2" />
+        <p className="h-24">
+          {description.substring(0, 200) +
+            (() => {
+              if (description.length > 200) return "...";
+              else return "";
+            })()}
+        </p>
+        <p className="text-xs text-right">{pubDate}</p>
+      </div>
+      <div className="text-center mt-3">
+        <a
+          href={postUrl}
+          className="bg-blue-500 text-white font-bold px-7 py-2 rounded-lg hover:bg-blue-600"
+          target="_blank"
+        >
+          See More ➡️{" "}
+        </a>
+      </div>
+    </div>
+  );
+}
