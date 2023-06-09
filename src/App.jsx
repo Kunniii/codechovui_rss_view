0c24b6ac0c9c71b997c1a4595d7a3d49a6be6e52
Kunniii <nguyenairport56@gmail.com>
2023-06-09 10:08:22 +0700

    sadfawefaweff

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ export default function App() {
   return (
     <div className="bg-slate-900 select-none">
       <h1 className="text-center text-white pt-24 font-bold text-7xl">RSS Code Cho Vui</h1>
-      <div className="grid grid-cols-3 sm:grid-cols-1 px-64 py-20">
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 lg:px-64 md:px-32 sm:px-10 py-20">
         {posts.map((post) => (
           <Card {...post} />
         ))}
